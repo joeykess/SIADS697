223b70d5a4b546f1a7187f0d448db8b785bb66d7
Joey Kessler <joeykess@umich.edu>
2021-05-03 21:58:09 -0700

    Adding stock info script. Call it and it will add '/assets' folder if it does not exist, then get DJI historical ticker information.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SIADS697
+
+### Gathering DJI Stock historical data
+
+This code will be using the python package yfinance to gather all DJI historical daily data.
---
+++ historical_stocks.py
@@ -0,0 +1,31 @@
+import time
+import yfinance as yf
+import os
+
+DJI_stocks = ['AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS', 'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM',
+              'MCD', 'MMM', 'MRK', 'MSFT', 'NKE', 'PG', 'TRV', 'UNH', 'CRM', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'DOW']
+
+
+def get_stock_info(symbol_list):
+    """
+    This function outputs all csv files to /assets/ directory as individual CSV files per stocks
+    :param symbol_list: list of stock symbols to get historical data on
+    """
+    for ticker in symbol_list:
+        stock = yf.Ticker(ticker)
+        hist = stock.history(period='max')
+        hist['sector'] = stock.info['sector']
+        hist['ticker'] = ticker
+        csv_name = 'assets/' + ticker + '.csv'
+        hist.to_csv(csv_name)
+        print("Saved file for ", ticker)
+        time.sleep(5)
+    print("Done!")
+
+
+if __name__ == '__main__':
+    cwd = os.getcwd()
+    path = os.path.join(cwd, '/assets')
+    if not os.path.exists(path):
+        os.mkdir(path)
+    get_stock_info(DJI_stocks)
