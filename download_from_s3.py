380fdb203d4f861ebb96e82977ccc3cf11548dd4
Joey Kessler <joeykess@umich.edu>
2021-06-22 20:23:45 -0700

    download_from_s3.py updates plus beginning skeleton to portfolio.py

--- download_from_s3.py
+++ download_from_s3.py
@@ -6,14 +6,16 @@ def download_files():
     Downloads all s3 files in bucket to local
     """
     bucket = "assets-bucket-q2wc9h6fcov44k5"
+    profile = input("Input your profile name")
+    boto3.setup_default_session(profile_name=profile)
     s3 = boto3.client('s3')
     results = s3.list_objects_v2(Bucket=bucket)
     results = [item['Key'] for item in results['Contents']]
-    path = os.path.join(os.getcwd(), '/assets')
+    path = os.path.join(os.getcwd(), 'assets')
     if not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
-    if not os.path.exists(os.path.dirname(path+'/historical-symbols')):
-        os.makedirs(os.path.dirname(path+'/historical-symbols'))
+    if not os.path.exists(os.path.dirname(path+'historical-symbols')):
+        os.makedirs(os.path.join(path, 'historical-symbols'))
     for folder in results:
         dest_path = path + '/' + folder
         s3.download_file(bucket, folder, dest_path)
--- portfolio.py
+++ portfolio.py
@@ -4,3 +4,27 @@
 # portfolio will have list of stocks (passed in)
 # based off price and weights we're using, trading bot will do whatever it wants to do
 # dump and re-balance every x_months
+import pandas as pd
+
+
+class portfolio:
+    def __init__(self, value):
+        self.tickers = {}
+        self.portfolio_value = value
+        self.cash = value
+
+    def buy(self, purchase_order, date):
+        """
+        :param purchase_order:{ticker:amount}
+        :param date: mm/dd/yyyy
+        """
+        for p_ticker, p_price in purchase_order:
+            df = pd.read_csv('assets/historical-symbols/{}.csv'.format(p_ticker))
+
+    def sell(self, sell_order, date):
+        """
+        :param sell_order: {ticker:amount}
+        :param date: mm/dd/yyyy
+        """
+        for s_ticker, s_price in sell_order:
+            df = pd.read_csv('assets/historical-symbols/{}.csv'.format(s_ticker))
