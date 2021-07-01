a45726a9d935fb86a57ca6e2b54a2034b742d6bf
Joey Kessler <joeykess@umich.edu>
2021-06-30 22:20:24 -0700

    chart_creator.py changes

--- chart_creator.py
+++ chart_creator.py
@@ -3,17 +3,76 @@ import pandas as pd
 
 
 def candle_charts(ticker, start, end):
-    df = pd.read_csv(f'assets/historical-symbols/{ticker}.csv')
-    df['MA10'] = df['Close'].rolling(window=10).mean()
-    df['MA60'] = df['Close'].rolling(window=60).mean()
-    df = df[(df['Date'] >= start) & (df['Date'] <= end)]
-    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
-                open=df['Open'],
-                high=df['High'],
-                low=df['Low'],
-                close=df['Close'])])
+    df2 = pd.read_csv(f'assets/historical-symbols/{ticker}.csv')
+    df2['MA15'] = df2['Close'].rolling(window=15).mean()
+
+    def bollinger_bands(data, sma, window):
+        std = data.rolling(window=window).std()
+        upper_bb = sma + std * 2
+        lower_bb = sma - std * 2
+        return upper_bb, lower_bb
+
+    df2['upper_bb'], df2['lower_bb'] = bollinger_bands(df2['Close'], df2['MA15'], 15)
+    df = df2[(df2['Date'] >= start) & (df2['Date'] <= end)]
+    candle = {
+        "name": "GS",
+        "type": "candlestick",
+        "x": df['Date'],
+        "yaxis": "y2",
+        "low": df['Low'],
+        "high": df['High'],
+        "open": df['Open'],
+        "close": df['Close'],
+        "decreasing": {"line": {"color": "#7F7F7F"}},
+        "increasing": {"line": {"color": "#17BECF"}}
+    }
+    moving_avg = {
+        "line": {"width": 3},
+        "mode": "lines",
+        "name": "Moving Average",
+        "type": "scatter",
+        "x": df['Date'],
+        "y": df['MA15'],
+        "yaxis": "y2",
+        "marker": {"color": "#E377C2"}
+    }
+    bband_up = {
+        "line": {"width": 3},
+        "name": "Bollinger Bands",
+        "mode": "lines",
+        "type": "scatter",
+        "x": df['Date'],
+        "y": df['upper_bb'],
+        "yaxis": "y2",
+        "marker": {"color": "#ccc"},
+        "hoverinfo": "none",
+        "legendgroup": "Bollinger Bands"
+    }
+    bband_down = {
+        "line": {"width": 3},
+        "mode": "lines",
+        "type": "scatter",
+        "x": df['Date'],
+        "y": df['lower_bb'],
+        "yaxis": "y2",
+        "marker": {"color": "#ccc"},
+        "hoverinfo": "none",
+        "showlegend": False,
+        "legendgroup": "Bollinger Bands"
+    }
+    layout = go.Layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis={"visible": False, "showticklabels": False, "showgrid": False,
+               "rangebreaks": [
+                   dict(bounds=["sat", "mon"]),  # hide weekends
+                   # https://plotly.com/python/time-series/#hiding-weekends-and-holidays
+               ]},
+        yaxis={"visible": False, "showticklabels": False, "showgrid": False}
+    )
+    fig = go.Figure(data=[candle, moving_avg, bband_up, bband_down], layout=layout)
+    fig.update_layout(showlegend=False)
     fig.show()
 
 
 if __name__ == '__main__':
-    candle_charts('BA', '2015-01-01', '2015-01-31')
+    candle_charts('BA', '2015-01-01', '2015-01-15')
