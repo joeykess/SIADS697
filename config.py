efeba7247657933852f49e10fd761474d0e35be4
gardenj79 <73771265+gardenj79@users.noreply.github.com>
2021-07-31 23:11:19 -0700

    converted to sql data storage for factor model

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#eikon
+def ek_key ():
+    eikon_key='b4229eae2c644ec8a0aab0bb0fb83b13a38eb2c7'
+    return eikon_key
+
+def username_ds():
+    username_ds = 'JGarden1@lidoadvisors.com'
+    return username_ds
+
+def pw_ds():
+    pw_ds = 'Welcome2'
+    return pw_ds
+
+if __name__ == "__main__":
+    ek_key()
+    username_ds()
+    pw_ds()
