2d41ca2b5226735a2b0e4c200ea0326a7204a1a6
joey <joeykess@umich.edu>
2021-08-01 18:43:46 -0700

    Intraday portfolio modeling

--- intraday_portfolio.py
+++ intraday_portfolio.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas_market_calendars as mcal
 import plotly.express as px
 import mplfinance as mpf
+import random
 import matplotlib
 
 matplotlib.use('agg')
@@ -12,11 +13,11 @@ import shutil
 from tqdm import tqdm
 import numpy as np
 # import tensorflow as tf
-# from tensorflow import keras
+from tensorflow import keras
 from keras.preprocessing import image
 import warnings
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=[FutureWarning, UserWarning])
 
 
 def create_tracking_df():
@@ -55,6 +56,7 @@ def bollinger_bands(data, sma, window):
 
 
 def create_candles(plot_df, file):
+    plot_df = plot_df.drop(columns=['volume'])
     if not os.path.exists('assets/models/joey_cnn_intraday/live_test'):
         os.mkdir('assets/models/joey_cnn_intraday/live_test')
     fig, ax = plt.subplots(figsize=(5, 5))
@@ -75,7 +77,7 @@ def create_candles(plot_df, file):
 
 
 class intraday_portfolio:
-    def __init__(self, start_date='2020-07-20', value=1000000, end_date='2021-07-20'):
+    def __init__(self, start_date='2020-07-20 04:05:00', value=1000000, end_date='2021-07-20'):
         """
         :param start_date: beginning date to start portfolio
         :param value: amount of initial cash to use to buy shares
@@ -95,7 +97,7 @@ class intraday_portfolio:
             'Date', 'Order Type', 'Ticker', 'Quantity', 'Ticker Value', 'Total Trade Value', 'Remaining Cash'
         ])
         self.hist_cash_df = pd.DataFrame([[start_date, value]], columns=['Date', 'Cash Available to Trade'])
-        self.hist_cash_dict = {datetime.strptime(start_date, '%Y-%m-%d'): value}
+        self.hist_cash_dict = {start_date: value}
         self.tracking_df = create_tracking_df()
 
     def reset_account(self):
@@ -132,8 +134,9 @@ class intraday_portfolio:
                      'Ticker': p_ticker, 'Quantity': p_order,
                      'Ticker Value': p_price, 'Total Trade Value': p_val,
                      'Remaining Cash': self.current_cash}, ignore_index=True)
-        self.hist_cash_dict[datetime.strptime(date, '%Y-%m-%d')] = self.current_cash
-        return
+        self.hist_cash_dict[date] = self.current_cash
+        self.hist_cash_df = self.hist_cash_df.append(
+            {'Date': date, 'Cash Available to Trade': self.current_cash}, ignore_index=True)
 
     def sell(self, sell_order, date):
         """
@@ -144,8 +147,8 @@ class intraday_portfolio:
         # goes through order and sells all necessary stocks
         for s_ticker, s_order in sell_order.items():
             s_price = self.get_price(date=date, symb=s_ticker)
-            s_val = s_order * s_price
-            if s_ticker in self.open_positions_dict:
+            if s_ticker in self.open_positions_dict and self.open_positions_dict[s_ticker] > 0:
+                s_val = s_order * s_price
                 self.open_positions_dict[s_ticker] -= s_order
                 self.current_cash += s_val
                 self.hist_trades_df = self.hist_trades_df.append(
@@ -153,30 +156,21 @@ class intraday_portfolio:
                      'Ticker': s_ticker, 'Quantity': s_order,
                      'Ticker Value': s_price, 'Total Trade Value': s_val,
                      'Remaining Cash': self.current_cash}, ignore_index=True)
-        self.hist_cash_dict[datetime.strptime(date, '%Y-%m-%d')] = self.current_cash
-
-    def sell_open_position(self, tickers, date):
-        """
-        sells all open positions for specific tickers
-        :param tickers: list of tickers - or single ticker as string
-        :param date: date selling
-        """
-        if type(tickers) == str:
-            self.sell(sell_order=self.open_positions_dict[tickers], date=date)
-        else:
-            for ticker in tickers:
-                self.sell(sell_order=self.open_positions_dict[ticker], date=date)
+        self.hist_cash_dict[date] = self.current_cash
+        self.hist_cash_df = self.hist_cash_df.append(
+            {'Date': date, 'Cash Available to Trade': self.current_cash}, ignore_index=True)
 
     def sell_all(self, date):
         """
         liquidates all open positions
         """
-        self.sell(sell_order=self.open_positions_dict, date=date)
+        sell_order = self.open_positions_dict
+        self.sell(sell_order=sell_order, date=date)
 
     def view_trade_history(self):
         return self.hist_trades_df.set_index('Date')
 
-    def calculate_daily_returns(self, end_date='2021-07-02'):
+    def calculate_daily_returns(self, end_date='2021-07-23'):
         """
         :param end_date: date is hard coded to last date in data
         """
@@ -186,37 +180,19 @@ class intraday_portfolio:
         d_range = mcal.date_range(early, frequency='1D').normalize().date
         end_date = datetime.strptime(end_date, '%Y-%m-%d')
         start_date = datetime.strptime(self.start_date, '%Y-%m-%d')
-        # gets all historical data from starting date to ending date and then sets index to date in datetime format
-        hist_df = self.tracking_df[self.tracking_df.index <= end_date]
-        hist_df = hist_df[hist_df.index >= start_date]
-        hist_df = self.hist_trades_df.set_index('Date')
-        hist_df.index = pd.to_datetime(hist_df.index)
-        # gets all cash extended out from all trading days
-        cash_df = pd.DataFrame.from_dict(self.hist_cash_dict, orient='index', columns=['Cash Available to Trade'])
-        cash_df = cash_df.reindex(pd.date_range(self.start_date, end_date, freq='B')).ffill()
+        cash_df = self.hist_cash_df.set_index('Date').iloc[1:]
         cash_df.index = pd.to_datetime(cash_df.index)
-        # gets all historical trades
-        ticker_df = pd.DataFrame.from_dict(self.hist_trades_dict, orient='index')
-        ticker_df.index = pd.to_datetime(ticker_df.index)
-        ticker_df = ticker_df.reindex(pd.date_range(start_date, end_date, freq='B')).ffill()
-        # muted FutureWarning: Indexing a timezone-naive DatetimeIndex with a timezone-aware datetime is
-        # deprecated and will raise KeyError in a future version.  Use a timezone-naive object instead.
-        ticker_df.index = pd.DatetimeIndex(ticker_df.index)
-        ticker_df.index = ticker_df.index.normalize()
-        ticker_df = ticker_df.loc[d_range]
-        # multiplies the stock value at whatever date to how many you own
-        ticker_df = ticker_df.mul(self.tracking_df, fill_value=0)
-        ticker_df = ticker_df.fillna(0).loc[start_date:end_date]
-        ticker_vals = pd.DataFrame(ticker_df.sum(axis=1), columns=['Stock Value'])
-        # concats the stock value and cash dataframes together then does easy math to get ROI and total value
-        returns_df = pd.concat([ticker_vals, cash_df], axis=1)
-        returns_df['Total Portfolio Value'] = returns_df['Stock Value'] + returns_df['Cash Available to Trade']
-        returns_df['Rate of Return'] = returns_df['Total Portfolio Value'].apply(lambda x: (x - self.init_cash) / x)
-        returns_df['Rate of Return'] = returns_df['Rate of Return'].apply(lambda x: "{0:.2f}%".format(x * 100))
-        returns_df = returns_df.dropna()
+        print(cash_df)
+        cash_df = cash_df.groupby(pd.Grouper(freq='D')).last()
+        # cash_df = cash_df.reindex(pd.date_range(self.start_date, end_date, freq='B')).ffill()
+
+        cash_df['Total Portfolio Value'] = cash_df['Cash Available to Trade']
+        cash_df['Rate of Return'] = cash_df['Total Portfolio Value'].apply(lambda x: (x - self.init_cash) / x)
+        cash_df['Rate of Return'] = cash_df['Rate of Return'].apply(lambda x: "{0:.2f}%".format(x * 100))
+        cash_df = cash_df.dropna()
 
         # formatting from Jeff's figures
-        returns_fig = px.line(returns_df['Rate of Return'].apply(lambda x: float(str(x).strip('%')) / 100),
+        returns_fig = px.line(cash_df['Rate of Return'].apply(lambda x: float(str(x).strip('%')) / 100),
                               title='Returns on Initial Investment', width=700, height=500)
         returns_fig.update_layout(width=950, height=300,
                                   margin=dict(l=20, r=20, t=50, b=10),
@@ -227,64 +203,81 @@ class intraday_portfolio:
                                   title=dict(text='Performance Chart', font=dict(size=20, color='black'), x=0.5,
                                              y=0.96))
         returns_fig.show()
-        return returns_df
+        return cash_df
 
 
-def intraday_trading(pf, model_path, weights_path):
-    # model = keras.models.load_model(model_path)
-    # model.set_weights(weights_path)
-
+def intraday_trading(pf, model_path):
+    model = keras.models.load_model(model_path)
+    path = 'assets/models/joey_cnn_intraday/live_test'
     start_date = '2020-07-20 04:05:00'
+    test_end_date = '2020-12-31 20:00:00'
     # loop through array above in portfolio (pf)
-    df = pf.tracking_df.loc[start_date:]
+    df = pf.tracking_df.loc[start_date:test_end_date]
     for time in tqdm(range(0, df.shape[0])):
         # for each trading period, after trading delete folder contents
         # get predictions here
         trading_time = df.iloc[time].name.hour >= 5 and df.iloc[time].name.minute % 15 == 0
+        test = df.iloc[time].name
         closing = df.iloc[time].name.minute >= 0 and df.iloc[time].name.hour >= 20
         if trading_time and not closing:
-            pf.sell_all(time)
             if os.path.exists('assets/models/joey_cnn_intraday/live_test'):
                 shutil.rmtree('assets/models/joey_cnn_intraday/live_test')
-            tickers = ['NVDA', 'AMD', 'JPM', 'JNJ', 'MRNA', 'F', 'TSLA', 'MSFT', 'BAC', 'BABA', 'SPY', 'QQQ']
-            timed_df = df.iloc[time - 12: time].copy()
+            tickers = ['SPY', 'QQQ', 'NVDA', 'AMD', 'JPM', 'JNJ', 'MRNA', 'F', 'TSLA', 'MSFT', 'BAC', 'BABA']
+            plotted_tickers = []
+            timed_df = df.iloc[:time + 1].copy()
             for symb in tickers:
-                symbol_df = timed_df[[f'{symb}_open', f'{symb}_high', f'{symb}_low', f'{symb}_close']]
+                symbol_df = timed_df[[f'{symb}_open', f'{symb}_high', f'{symb}_low', f'{symb}_close', f'{symb}_volume']]
                 symbol_df = symbol_df.rename(columns={f'{symb}_open': 'open', f'{symb}_high': 'high',
-                                                      f'{symb}_low': 'low', f'{symb}_close': 'close'})
+                                                      f'{symb}_low': 'low', f'{symb}_close': 'close',
+                                                      f'{symb}_volume': 'volume'})
+                symbol_df['MA12'] = symbol_df['close'].rolling(window=12).mean()
+                symbol_df['upper_bb'], symbol_df['lower_bb'] = bollinger_bands(symbol_df['close'], symbol_df['MA12'],
+                                                                               12)
+                symbol_df['ema12'] = symbol_df['close'].ewm(span=12).mean()
+                if time > 12:
+                    symbol_df = symbol_df.iloc[-12:]
+                if symbol_df['MA12'].isna().sum() > 1:
+                    continue
                 if 0.0 in symbol_df['volume'].value_counts():
                     if symbol_df['volume'].value_counts()[0.0] <= 4:
                         create_candles(plot_df=symbol_df, file=str(tickers.index(symb)))
+                        plotted_tickers.append(symb)
                 else:
+                    plotted_tickers.append(symb)
                     create_candles(plot_df=symbol_df, file=str(tickers.index(symb)))
-            images = []
-            path = 'assets/models/joey_cnn_intraday/live_test'
-            width, height = 203, 202
-            # gets all images and stacks them together for predictions
-            for img in os.listdir(path):
-                img = os.path.join(path, img)
-                img = image.load_img(img, target_size=(width, height))
-                img = image.img_to_array(img)
-                img = np.expand_dims(img, axis=0)
-                images.append(img)
-            images = np.vstack(images)
-            preds = model.predict_classes(images, batch_size=12)  # will output 1 for buy and 0 for hold
-            cash = pf.current_cash
-            preds_buy = [tickers[i] for i, x in enumerate(preds) if x == 1]
-            buy_order = {}
-            for item in preds_buy:
-                max_cost = cash * 0.75  # trading max 75% cash every trade
-                cost_per = max_cost / len(preds_buy)
-                price = pf.get_price(date=time, symb=item)
-                buy_order['item'] = int(np.floor(cost_per / price))
-            pf.buy(purchase_order=buy_order, date=time)
+            if os.path.exists(path):
+                images = []
+                width, height = 203, 202
+                # gets all images and stacks them together for predictions
+                for img in os.listdir(path):
+                    img = os.path.join(path, img)
+                    img = image.load_img(img, target_size=(width, height))
+                    img = image.img_to_array(img)
+                    img = np.expand_dims(img, axis=0)
+                    images.append(img)
+                images = np.vstack(images)
+                preds = np.argmax(model.predict(images), axis=-1)  # will output 0 for buy and 1 for hold
+                pf.sell_all(df.iloc[time].name)
+                cash = pf.current_cash
+                preds_buy = [plotted_tickers[i] for i, x in enumerate(preds) if x == 0]
+                buy_order = {}
+                max_cost = cash * 0.75
+                if len(preds_buy) > 0:
+                    cost_per = max_cost / len(preds_buy)
+                    for item in preds_buy:
+                        # trading max 75% cash every trade
+                        price = pf.get_price(date=df.iloc[time].name, symb=item)
+                        buy_order[item] = int(np.floor(cost_per / price))
+                    pf.buy(purchase_order=buy_order, date=df.iloc[time].name)
         if closing:
-            pf.sell_all(time)
+            pf.sell_all(df.iloc[time].name)
+            print(pf.current_cash)
 
 
 if __name__ == '__main__':
     # commence example portfolio with $100k on 2020-07-20
     model = 'assets/models/joey_cnn_intraday/cnn_model_100epochs_2classes.h5'
-    weights = 'assets/models/joey_cnn_intraday/cnn_weights_100epochs_2classes.h5'
     ptflio = intraday_portfolio(start_date='2020-07-20', value=100000)
-    intraday_trading(ptflio, model_path=model, weights_path=weights)
+    intraday_trading(ptflio, model_path=model)
+    print(ptflio.current_cash)
+    print(ptflio.calculate_daily_returns())
